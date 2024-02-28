649516a57cd2441aefc500eb127f88b60b0a162b
arhturfary <arthurfary22@gmail.com>
2024-02-28 13:49:26 -0300

    base para calendario

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { useState } from "react";
+
 
 const CalendarBg = styled.div`
     background-color: red;
@@ -6,14 +8,69 @@ const CalendarBg = styled.div`
     min-height: 300px;
 `
 
-const Calendar = () => {
+const CalendarItem = styled.div`
+  display: flex;
+  flex-direction: column;
+   
+  background-color: #fcfcfc;
+  width: 100%;
+  height: fit-content;
+
+
+`
+
+function Calendar() {
+   // Function to convert a datetime object to days and months (helper function)
+  const datetimeToDaysMonths = (dt: Date) => {
+    return { day: dt.getDate(), month: dt.getMonth() + 1 }; // Months are 0-indexed, so add 1
+  };
+
+  interface ScheduleItem {
+        date: Date;
+        description: string;
+        name: string;
+        place: string
+    }
+
+  const dates: ScheduleItem[] = [];
+  dates.push(
+    {
+        "date": new Date(Date.now()),
+        "description": "teste",
+        "name": "nome do evento",
+        "place": "lugar" 
+    })
+
+
+  const scheduleItems = dates.map((item: ScheduleItem)=> {
+    // Extract day and month for formatted display
+    // const { day, month } = datetimeToDaysMonths(item.date);
+
     return (
-        <>
-            <CalendarBg>
-                <h1>testing </h1>
-            </CalendarBg>
-        </>
-    )
+      <CalendarItem>
+        <p>
+          <b>Data:</b> {item.date.toLocaleDateString()}
+        </p>
+
+        <p>
+          <b>Nome:</b> {item.name} - <b>{item.place}</b> 
+        </p>
+
+        <p>
+          <b>Descrição:</b> {item.description}
+        </p> 
+      </CalendarItem>
+    );
+  });
+
+  return (
+    <div>
+        {/* UI elements to control start date and number of days (optional) */}
+        <CalendarBg>        
+        {scheduleItems}
+        </CalendarBg>
+    </div>
+  );
 }
 
-export default Calendar
+export default Calendar;
