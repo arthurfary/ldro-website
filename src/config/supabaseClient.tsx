8658d3bb84374d229251ae694ac2728a475130c9
arthurfary <arthurfary22@gmail.com>
2024-03-27 15:19:10 -0300

    changed env names

--- src/config/supabaseClient.tsx
+++ src/config/supabaseClient.tsx
@@ -3,9 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 let supabaseUrl: string;
 let supabaseKey: string;
 
-if (process.env.REACT_APP_SUPABASE_URL && process.env.REACT_APP_ANON_KEY){
+if (process.env.REACT_APP_SUPABASE_URL && process.env.REACT_APP_SUPABASE_ANON_KEY){
   supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-  supabaseKey = process.env.REACT_APP_ANON_KEY;
+  supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 } else {
   throw new Error("Env Variables are not set")
 }
